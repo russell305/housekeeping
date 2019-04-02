8b890c5ca3f0ff633798793374cad6d161c6f6c6
Russell Mckee <russm305@gmail.com>
2019-04-02 16:22:05 -0400

    nnnn

--- application.py
+++ application.py
@@ -20,9 +20,7 @@ from flask_sslify import SSLify
 
 app = Flask(__name__) # Instantiate a new web application called `app`, with `__name__` representing the current file
 # Talisman(app)
-sslify = SSLify(app, age=300)
-csrf = SeaSurf(app)
-Session (app)
+
 UPLOAD_FOLDER = '/static/image'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -35,7 +33,9 @@ GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-
+sslify = SSLify(app, age=300)
+csrf = SeaSurf(app)
+Session (app)
 engine = create_engine("postgres://ayjxjjxhgpzlnl:f150cc319da46e38a1fb398ee335d98fa5468668d0d8aa3da415aed475d08f9b@ec2-54-225-227-125.compute-1.amazonaws.com:5432/d9prh5mib7dh2p")
 #talk to datbase wiTh SQL. Object used to manage connections to database.
 #Sending data to and from database
